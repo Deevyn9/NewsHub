30a76f001e7aa9bee167be74db00534fe490c86c
Divine Obeten <divineobeten9@gmail.com>
2024-03-27 13:21:40 +0100

    add components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 // import { useState } from 'react'
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Personalized from "./components/Personalized";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      {/* inside router-ish */}
+      <Home />
+      <Personalized />
+      {/* inside router-ish */}
+    </>
+  );
 }
 
 export default App;
