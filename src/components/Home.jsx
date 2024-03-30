acc3ff138c7359a4d77effb6273f89e36690ad0b
Divine Obeten <divineobeten9@gmail.com>
2024-03-30 21:59:36 +0100

    display articles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({ articles, addToPersonalized }) => {
   return (
     <div className="main-area">
       <div className="heading">
@@ -9,39 +9,41 @@ const Home = () => {
       </div>
 
       <div className="news-container">
-        <div className="article">
-          <div className="article-image">
-            <img
-              src="https://st2.depositphotos.com/1561359/12101/v/950/depositphotos_121012076-stock-illustration-blank-photo-icon.jpg"
-              alt="article image"
-            />
-          </div>
-          <div className="article-details">
-            <div className="article-source">
-              <div className="source-headline">
-                <div className="source-image">
-                  <img
-                    src="https://st2.depositphotos.com/1561359/12101/v/950/depositphotos_121012076-stock-illustration-blank-photo-icon.jpg"
-                    alt="source image"
-                  />
-                  <p>Vanguard</p>
+        {articles.map((article) => (
+          <div className="article" key={article.url + 1}>
+            <div className="article-details">
+              <div className="article-source">
+                <div className="source-headline">
+                  <div className="source-image">
+                    <img
+                      src="https://st2.depositphotos.com/1561359/12101/v/950/depositphotos_121012076-stock-illustration-blank-photo-icon.jpg"
+                      alt="source image"
+                    />
+                    <p>{article.source.name}</p>
+                  </div>
+                </div>
+                <a href={article.url} target="_blank">
+                  <div className="article-headline">
+                    <h3>{article.title}</h3>
+                  </div>
+                </a>
+              </div>
+
+              <div className="article-card__bottom">
+                <div className="name-date">
+                  <div className="article-date">{article.publishedAt}</div>
+                  <div className="divider">•</div>
+                  <div className="article-writer">{article.author}</div>
+                </div>
+                <div className="preference-button">
+                  <button onClick={() => addToPersonalized(article)}>
+                    Add to Preference
+                  </button>
                 </div>
               </div>
-              <div className="article-headline">
-                <h3>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aspernatur pariatur quis laboriosam, aperiam ab a!
-                </h3>
-              </div>
-            </div>
-
-            <div className="name-date">
-              <div className="article-date">3 days ago </div>
-              <div className="divider">•</div>
-              <div className="article-writer"> Samm Abdu</div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
